a4ecd38ef638a09f3486f72e161c94a62b142423
Nhahan <kisy324@naver.com>
2024-11-20 20:57:25 +0900

    init

---
+++ eslint.config.cjs
@@ -0,0 +1,42 @@
+const { FlatCompat } = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+});
+
+module.exports = [
+    {
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                ecmaVersion: 2021,
+                sourceType: "module",
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        plugins: {
+            react: require("eslint-plugin-react"),
+            "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+            "react-hooks": require("eslint-plugin-react-hooks"),
+            "jsx-a11y": require("eslint-plugin-jsx-a11y"),
+            prettier: require("eslint-plugin-prettier"),
+        },
+        rules: {
+            "prettier/prettier": "error",
+            "react/react-in-jsx-scope": "off",
+            "react/prop-types": "off",
+            "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+            "@typescript-eslint/explicit-module-boundary-types": "off",
+        },
+        settings: {
+            react: {
+                version: "detect",
+            },
+        },
+    },
+    ...compat.extends("plugin:react/recommended"),
+    ...compat.extends("plugin:@typescript-eslint/recommended"),
+];
